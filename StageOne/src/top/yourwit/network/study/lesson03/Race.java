be5313f4b83d154bd7546a0acd84068726b88fe6
comdotwww <wang_zha@outlook.com>
2020-05-10 16:51:56 +0800

    Creat Project

--- StageOne/src/top/yourwit/network/study/lesson03/Race.java
+++ StageOne/src/top/yourwit/network/study/lesson03/Race.java
@@ -1,9 +1,16 @@
 package top.yourwit.network.study.lesson03;
 
 //  龟兔赛跑
-public class Race implements Runnable{
+public class Race implements Runnable {
     @Override
     public void run() {
 //
+
+    }
+
+
+    public static void main(String[] args) {
+        Race race = new Race();
+        new Thread(race,"Test");
     }
 }
