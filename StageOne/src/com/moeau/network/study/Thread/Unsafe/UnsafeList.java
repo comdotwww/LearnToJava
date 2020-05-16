2746f6811809eee592a3a0d6dbc8692c1b78da62
comdotwww <wang_zha@outlook.com>
2020-05-16 22:41:06 +0800

    add UnsafeList

---
+++ StageOne/src/com/moeau/network/study/Thread/Unsafe/UnsafeList.java
@@ -0,0 +1,23 @@
+package com.moeau.network.study.Thread.Unsafe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnsafeList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10000; i++) {
+            new Thread(
+                    ()->{
+                        list.add(Thread.currentThread().getName());
+                    }
+            ).start();
+        }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(list.size());
+    }
+}
